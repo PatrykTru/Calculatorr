97b367ab099750dfe02ab28e64aa70748219d871
PatrykTru <patryk.truchan94@gmail.com>
2022-01-27 15:35:25 +0100

    finish

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,23 @@
 public class Calculator {
 
+    public int add(int a , int b)
+    {
+        return a+b;
+    }
+
+    public int minus(int a  , int b)
+    {
+        return a-b;
+
+    }
+
 
     public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(2,2));
+        System.out.println(calculator.minus(5,3));
+
 
 
     }
